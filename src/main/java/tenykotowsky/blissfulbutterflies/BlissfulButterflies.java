42773a53c7e5ade88968f3ef7c43f11a8355dfd9
TenKotowsky <fairoutak@gmail.com>
2023-07-05 16:19:30 +0200

    Fixed some problems

--- src/main/java/tenykotowsky/blissfulbutterflies/BlissfulButterflies.java
+++ src/main/java/tenykotowsky/blissfulbutterflies/BlissfulButterflies.java
@@ -14,7 +14,7 @@ public class BlissfulButterflies implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final String MOD_ID = "blissfulbutterflies";
+	public static final String MOD_ID = "blissful-butterflies";
     public static final Logger LOGGER = LoggerFactory.getLogger("blissful-butterflies");
 
 	@Override
