4573c3fc284afc88df36cac3d4ef32f9875d06aa
TenKotowsky <fairoutak@gmail.com>
2023-07-10 12:53:22 +0200

    Butterfly Net fix

--- src/main/java/tenykotowsky/blissfulbutterflies/item/custom/ButterflyNet.java
+++ src/main/java/tenykotowsky/blissfulbutterflies/item/custom/ButterflyNet.java
@@ -1,34 +1,12 @@
 package tenykotowsky.blissfulbutterflies.item.custom;
 
-import com.mojang.brigadier.Command;
-import com.mojang.brigadier.ParseResults;
-import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
-import net.minecraft.block.entity.BlockEntity;
-import net.minecraft.block.entity.MobSpawnerBlockEntity;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityType;
-import net.minecraft.entity.SpawnReason;
-import net.minecraft.entity.data.DataTracker;
-import net.minecraft.entity.data.TrackedData;
-import net.minecraft.entity.data.TrackedDataHandlerRegistry;
 import net.minecraft.item.*;
 import net.minecraft.nbt.NbtCompound;
-import net.minecraft.nbt.NbtElement;
-import net.minecraft.network.PacketByteBuf;
-import net.minecraft.network.message.SentMessage;
-import net.minecraft.network.message.SignedMessage;
-import net.minecraft.network.packet.c2s.play.ClientCommandC2SPacket;
-import net.minecraft.registry.RegistryKeys;
-import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.command.CommandManager;
-import net.minecraft.server.command.ServerCommandSource;
-import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
-import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
@@ -39,8 +17,6 @@ import tenykotowsky.blissfulbutterflies.entity.ModEntities;
 import tenykotowsky.blissfulbutterflies.entity.custom.ButterflyEntity;
 import tenykotowsky.blissfulbutterflies.entity.variant.ButterflyVariant;
 
-import javax.imageio.plugins.tiff.BaselineTIFFTagSet;
-import java.util.Objects;
 public class ButterflyNet extends Item {
     public ButterflyNet(Settings settings) {
         super(settings);
@@ -59,27 +35,25 @@ public class ButterflyNet extends Item {
             BlockState blockState = world.getBlockState(blockPos);
 
             BlockPos blockPos2 = blockState.getCollisionShape(world, blockPos).isEmpty() ? blockPos : blockPos.offset(direction);
-            String variant = itemStack.getOrCreateNbt().get("Variant").toString();
+            String variant = itemStack.getOrCreateNbt().getString("Variant"); // Use getString instead of toString
             BlissfulButterflies.LOGGER.info(variant);
 
             ButterflyEntity butterfly = new ButterflyEntity(ModEntities.BUTTERFLY, world);
-            NbtCompound nbtComp = new NbtCompound();
             switch (variant) {
-                case "MONARCH" -> nbtComp.putInt("Variant", 0);
-                case "BLUE" -> nbtComp.putInt("Variant", 1);
-                case "PURPLE" -> nbtComp.putInt("Variant", 2);
-                case "ORANGE" -> nbtComp.putInt("Variant", 3);
-                case "WHITE" -> nbtComp.putInt("Variant", 4);
+                case "MONARCH" -> butterfly.setVariant(ButterflyVariant.MONARCH);
+                case "BLUE" -> butterfly.setVariant(ButterflyVariant.BLUE);
+                case "PURPLE" -> butterfly.setVariant(ButterflyVariant.PURPLE);
+                case "ORANGE" -> butterfly.setVariant(ButterflyVariant.ORANGE);
+                case "WHITE" -> butterfly.setVariant(ButterflyVariant.WHITE);
             }
-            butterfly.writeNbt(nbtComp);
 
-            butterfly.setPosition(blockPos2.getX(), blockPos2.getY(), blockPos2.getZ());
+            butterfly.setPosition(blockPos2.getX() + 0.5, blockPos2.getY(), blockPos2.getZ() + 0.5);
 
             world.spawnEntity(butterfly);
-            world.emitGameEvent((Entity)context.getPlayer(), GameEvent.ENTITY_PLACE, blockPos);
+            world.emitGameEvent((Entity) context.getPlayer(), GameEvent.ENTITY_PLACE, blockPos);
 
             itemStack.getOrCreateNbt().remove("Variant");
-            context.getPlayer().playSound(SoundEvents.BLOCK_BEEHIVE_EXIT, 1.0f, 1.0f);
+            context.getPlayer().playSound(SoundEvents.BLOCK_BEEHIVE_EXIT, SoundCategory.PLAYERS, 1.0f, 1.0f);
             return ActionResult.SUCCESS;
         }
         return ActionResult.PASS;
